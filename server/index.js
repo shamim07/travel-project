ba505dbe827287f1e5433b8b21da3392307f831c
shamim07 <111166994+shamim07@users.noreply.github.com>
2024-12-12 18:46:55 +0600

    versel url link

--- server/index.js
+++ server/index.js
@@ -28,9 +28,9 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      //"https://agency-blog-assignment.vercel.app",
-      "http://localhost:5173",
-      //CLIENT_URL,
+      "FRONT_URL",
+      //"http://localhost:5173",
+     
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
